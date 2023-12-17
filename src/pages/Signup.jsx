57679f2c98f1631a624ab3283111dc68298da03f
Wadad Parker <wadadparker5@gmail.com>
2023-12-17 07:48:56 +0530

    feat: signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="relative bg-[url(https://github.blog/wp-content/uploads/2020/12/102573561-8e872300-40a3-11eb-9feb-b480aeae0564.png)] bg-cover bg-center  w-screen h-screen">Signup
+        <h1 className='text-white text-7xl max-w-sm bottom-1/3 left-[10%] absolute font-black'>CREATE AN ACCOUNT</h1>
+    </div>
   )
 }
 
