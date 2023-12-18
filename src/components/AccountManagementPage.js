f3fe612f1d6c72e5162e56d23d3dad9eee1cf583
Karansenpai <karandeoli444@gmail.com>
2023-12-18 22:48:25 +0530

    edit

--- src/components/AccountManagementPage.js
+++ src/components/AccountManagementPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 
@@ -26,7 +26,7 @@ const left = {
 
 const right = {
   width: "100%",
-  flex: 4,
+  flex: 3,
 };
 
 const container = {
@@ -50,7 +50,7 @@ const innerRight = {
   display: "flex",
   flexDirection: "row",
   borderRadius: "2px",
-  padding: "5rem 1rem 2rem 5rem",
+  padding: "5rem 1rem 2rem 3rem",
   gap: "1rem",
 };
 
@@ -70,168 +70,179 @@ const H3 = {
 };
 
 const AccountManagementPage = () => {
-  return (
-    <div id="accountManagementPage" style={account}>
-      <div style={left}>
-        <div
-          style={{
-            padding: "1rem",
-          }}
-        >
-          <h1 style={H1}>
-            Account <br />
-            Management
-          </h1>
-        </div>
-        <div
-          style={{
-            padding: "1rem",
-          }}
-        >
-          <h3
+  const isMobile = useMediaQuery("(max-width: 700px)");
+
+  if (!isMobile) {
+    return (
+      <div id="accountManagementPage" style={account}>
+        <div style={left}>
+          <div
             style={{
-              fontFamily: font,
-              color: "#EBEAEA",
+              padding: "1rem",
             }}
           >
-            {<FaceRetouchingNaturalIcon />}
-            <span
+            <h1 style={H1}>
+              Account <br />
+              Management
+            </h1>
+          </div>
+          <div
+            style={{
+              padding: "1rem",
+            }}
+          >
+            <h3
               style={{
-                padding: "1rem",
-                position: "relative",
-                top: "-0.2rem",
+                fontFamily: font,
+                color: "#EBEAEA",
               }}
             >
-              Account Details
-            </span>
-          </h3>
-          <h3 style={H3}>
-            {<AssignmentIndIcon />}
-            <span
-              style={{
-                padding: "1rem",
-                position: "relative",
-                top: "-0.2rem",
-              }}
-            >
-              Account Activity
-            </span>
-          </h3>
+              {<FaceRetouchingNaturalIcon />}
+              <span
+                style={{
+                  padding: "1rem",
+                  position: "relative",
+                  top: "-0.2rem",
+                }}
+              >
+                Account Details
+              </span>
+            </h3>
+            <h3 style={H3}>
+              {<AssignmentIndIcon />}
+              <span
+                style={{
+                  padding: "1rem",
+                  position: "relative",
+                  top: "-0.2rem",
+                }}
+              >
+                Account Activity
+              </span>
+            </h3>
+          </div>
+        </div>
+        <div style={right}>
+          <section id="section1">
+            <div style={container}>
+              <div
+                style={{
+                  display: "flex",
+                  fiexDirection: "row",
+                  width: "95%",
+                  height: "40vh",
+                }}
+              >
+                <div style={innerLeft}>
+                  <h1 style={H1}>Account Details</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam
+                  </p>
+                </div>
+
+                <div style={innerRight}>
+                  <TextField
+                    label="NAME"
+                    InputProps={{
+                      style: {
+                        color: "#F2F2F2",
+                        fontFamily: font,
+                        background: "#333131",
+                        borderRadius: "8px",
+                        fontWeight: "bold",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "#999999",
+                        fontFamily: font,
+                        borderRadius: "8px",
+                        fontWeight: "bold",
+                        fontSize: "14px",
+                      },
+                    }}
+                  />
+                  <TextField
+                    label="USERNAME"
+                    InputProps={{
+                      style: {
+                        color: "#F2F2F2",
+                        fontFamily: font,
+                        background: "#333131",
+                        borderRadius: "8px",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "#999999",
+                        fontFamily: font,
+                        fontWeight: "bold",
+                        borderRadius: "8px",
+                        fontWeight: "bold",
+                        fontSize: "14px",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  fiexDirection: "row",
+                  width: "95%",
+                  height: "45vh",
+                }}
+              >
+                <div style={innerLeft}>
+                  <h1 style={H1}>Personal Information</h1>
+                  <p>
+                    This Information is private and will not be shared with
+                    others
+                  </p>
+                </div>
+                <div style={innerRight}>
+                  <TextField
+                    id="filled-basic"
+                    label="EMAIL ADDRESS"
+                    variant="filled"
+                    style={{
+                      width: "400px",
+                    }}
+                    InputProps={{
+                      style: {
+                        color: "#F2F2F2",
+                        fontFamily: font,
+                        background: "#452647",
+                        borderRadius: "8px",
+                      },
+                    }}
+                    InputLabelProps={{
+                      style: {
+                        color: "#CF51DA",
+                        fontFamily: font,
+                        fontWeight: "bold",
+                        borderRadius: "8px",
+                        fontSize: "14px",
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section id="section2"></section>
         </div>
       </div>
-      <div style={right}>
-        <section id="section1">
-          <div style={container}>
-            <div
-              style={{
-                display: "flex",
-                fiexDirection: "row",
-                width: "95%",
-                height: "40vh",
-              }}
-            >
-              <div style={innerLeft}>
-                <h1 style={H1}>Account Details</h1>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam
-                </p>
-              </div>
-
-              <div style={innerRight}>
-                <TextField
-                  label="NAME"
-                  InputProps={{
-                    style: {
-                      fontFamily: font,
-                      background: "#333131",
-                      borderRadius: "8px",
-                      fontWeight: "bold",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: "#999999",
-                      fontFamily: font,
-                      borderRadius: "8px",
-                      fontWeight: "bold",
-                      fontSize: "14px",
-                    },
-                  }}
-                />
-                <TextField
-                  label="USERNAME"
-                  InputProps={{
-                    style: {
-                      fontFamily: font,
-                      background: "#333131",
-                      borderRadius: "8px",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: "#999999",
-                      fontFamily: font,
-                      fontWeight: "bold",
-                      borderRadius: "8px",
-                      fontWeight: "bold",
-                      fontSize: "14px",
-                    },
-                  }}
-                />
-              </div>
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                fiexDirection: "row",
-                width: "95%",
-                height: "45vh",
-              }}
-            >
-              <div style={innerLeft}>
-                <h1 style={H1}>Personal Information</h1>
-                <p>
-                  This Information is private and will not be shared with others
-                </p>
-              </div>
-              <div style={innerRight}>
-                <TextField
-                  id="filled-basic"
-                  label="EMAIL ADDRESS"
-                  variant="filled"
-                  style={{
-                    width: "500px", 
-                  }}
-                  InputProps={{
-                    style: {
-                      color: "whited",
-                      fontFamily: font,
-                      background: "#452647",
-                      borderRadius: "8px",
-                    },
-                  }}
-                  InputLabelProps={{
-                    style: {
-                      color: "#CF51DA",
-                      fontFamily: font,
-                      fontWeight: "bold",
-                      borderRadius: "8px",
-                      fontSize: "14px",
-                      
-                    },
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section id="section2"></section>
-      </div>
-    </div>
-  );
+    );
+  }
+  else{
+   return <div>
+      Mobile h be
+   </div>
+  }
 };
 
 export default AccountManagementPage;
