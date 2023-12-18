d5ff0d144267a6f1e01d1e79dbc91c98d3d652a6
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-18 22:50:38 +0530

    Create ComposeScoopPage.js

---
+++ src/ComposeScoopPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ScoopForm from './ScoopForm';
+import './ComposeScoopPage.css'; 
+
+const ComposeScoopPage = () => {
+  return (
+    <div className="compose-scoop-page">
+      <h1>Compose Scoop</h1>
+      <ScoopForm />
+    </div>
+  );
+};
+
+export default ComposeScoopPage;
