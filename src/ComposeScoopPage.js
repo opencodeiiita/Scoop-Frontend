36c817051607a5b01d18a29fe5a1eedeb3d639e6
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-19 21:35:12 +0530

    Update ComposeScoopPage.js

--- src/ComposeScoopPage.js
+++ src/ComposeScoopPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './ComposeScoopPage.css';
 import ScoopForm from './ScoopForm';
-import './ComposeScoopPage.css'; 
 
 const ComposeScoopPage = () => {
+  const handleSubmit = values => {
+    // Handle submission logic here
+    console.log('Form submitted with values:', values);
+  };
+
   return (
     <div className="compose-scoop-page">
       <h1>Compose Scoop</h1>
-      <ScoopForm />
+      <ScoopForm onSubmit={handleSubmit} />
     </div>
   );
 };
