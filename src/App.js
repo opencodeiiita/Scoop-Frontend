46af2a9de92f0790142a443665c06266868d0f28
Aditya Khairnar <k7aditya@gmail.com>
2023-12-20 22:05:57 +0530

    Sign in page created + requested changes made

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import CategoryCarousel from './components/PopularPost.js';
 import Footer from './components/Footer.js';
 import Navbar from './components/Navbar.js';
 import ComposeScoopPage from './ComposeScoopPage.js';
+import Signinform from '/pages/SignInForm.js'
+
 function App() {
  
  return (
@@ -21,6 +23,7 @@ function App() {
        
        <Route path="/" element={<Home></Home>} />
        <Route path="*" element={<NotFound></NotFound>} />
+       <Route path="" element={<Signinform></Signinform>}/>
        
       </Routes>
       <Footer></Footer>
---
+++ src/pages/SignInForm.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import arrowImage from "../components/arrowicon.png";
+
+const Signinform = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    staySignedIn: false,
+  });
+
+  const [focusedInput, setFocusedInput] = useState({
+    email: false,
+    password: false,
+  });
+
+  const [signInError, setSignInError] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+
+    setFormData({
+      ...formData,
+      [name]: newValue,
+    });
+  };
+  const handleFocus = (field) => {
+    setFocusedInput({
+      ...focusedInput,
+      [field]: true,
+    });
+  };
+  const handleBlur = (field) => {
+    if (!formData[field]) {
+      setFocusedInput({
+        ...focusedInput,
+        [field]: false,
+      });
+    }
+  };
+
+  const handleSignIn = () => {
+    setSignInError(true);
+    if (formData.email === "abc@gmail.com" || formData.password === "abcxyz") {
+      setSignInError(false);
+      console.log("Signing in...");
+      //sign in logic
+    } else {
+      setSignInError(true);
+    }
+  };
+
+  return (
+    <div className="signinbox">
+      <h1 className="signin">Sign In</h1>
+
+      {/* <form onSubmit={handleSubmit}> */}
+      <div className="input-container">
+        {signInError && <p className="error-msg">Error Signing In!</p>}
+
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          onFocus={() => handleFocus("email")}
+          onBlur={() => handleBlur("email")}
+          placeholder="Username"
+          className={focusedInput.email || formData.email ? "focused" : ""}
+          style={{ borderColor: signInError ? "purple" : "" }}
+          required
+        />
+      </div>
+      <div className="input-container">
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onFocus={() => handleFocus("password")}
+          onBlur={() => handleBlur("password")}
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Password"
+          className={
+            focusedInput.password || formData.password ? "focused" : ""
+          }
+          style={{ borderColor: signInError ? "purple" : "" }}
+          required
+        />
+      </div>
+      <div className="checkbox">
+        <input
+          type="checkbox"
+          id="staySignedIn"
+          name="staySignedIn"
+          checked={formData.staySignedIn}
+          onChange={handleChange}
+        />
+        <label htmlFor="staySignedIn" className="staysignin">
+          Stay signed in
+        </label>
+      </div>
+
+      {/* </form> */}
+      <div className="bottom-section">
+        {
+          <div className="arrow-box" onClick={handleSignIn}>
+            <img src={arrowImage} alt="Arrow" />
+          </div>
+        }
+        <p className="belowbox">Can't Sign in?</p>
+        <p className="belowcant">Create Account</p>
+      </div>
+    </div>
+  );
+};
+
+export default Signinform;
