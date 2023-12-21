e75edb2c023f3f2673ed4d9fe5448ea2da9197b5
Om Buddhadev <buddhadevom@gmail.com>
2023-12-21 08:27:32 +0530

    n

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import {theme} from './theme.js';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import CategoryCarousel from './components/PopularPost.js';
+import Footer from './components/Footer.js';
+import Navbar from './components/Navbar.js';
+import ComposeScoopPage from './ComposeScoopPage.js';
+import Signinform from './pages/SignInForm.js'
+import AccountManagementPage from './components/AccountManagementPage.js';
+import ComposeScoopPage from './ComposeScoopPage.js'
+import Signup from './pages/Signup.jsx';
+
 function App() {
  
-  return (
-    <> 
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-       <BrowserRouter>
+ return (
+   <> 
+   <ThemeProvider theme={theme}>
+     <CssBaseline>
+      <BrowserRouter>
+      
+      <Routes>
        
-       <Routes>
-        
-        <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
-        <Route path="*" element={<NotFound></NotFound>} />
-        <Route path="/signin" element={<Signinform></Signinform>}/>
-        
-         <Route path="/signup" element={<Signup />} />
-           <Route path="/account" element={<AccountManagementPage/>} />
-       </Routes>
-       </BrowserRouter>
-       </CssBaseline>
-       </ThemeProvider>
-    
-    </>
-  );
- }
- 
- 
- export default App;
+       <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
+       <Route path="*" element={<NotFound></NotFound>} />
+       <Route path="" element={<Signinform></Signinform>}/>
+       
+        <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<AccountManagementPage/>} />
+      </Routes>
+      </BrowserRouter>
+      </CssBaseline>
+      </ThemeProvider>
+   
+   </>
+ );
+}
+
+
+export default App;
