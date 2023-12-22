5309b73c355d97b3763ef8cdfc218967cd8f2bf8
Aakash-1801 <randomb921@gmail.com>
2023-12-22 15:36:28 +0530

    allnewspage

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import React from 'react'
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import {theme} from './theme.js';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import CategoryCarousel from './components/PopularPost.js';
-import Footer from './components/Footer.js';
-import Navbar from './components/Navbar.js';
-import Signinform from './pages/SignInForm.js'
-import AccountManagementPage from './components/AccountManagementPage.js';
-import ComposeScoopPage from './ComposeScoopPage.js'
-import Signup from './pages/Signup.jsx';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "./theme.js";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import CategoryCarousel from "./components/PopularPost.js";
+import Footer from "./components/Footer.js";
+import Navbar from "./components/Navbar.js";
+import Signinform from "./pages/SignInForm.js";
+import AccountManagementPage from "./components/AccountManagementPage.js";
+import ComposeScoopPage from "./ComposeScoopPage.js";
+import Signup from "./pages/Signup.jsx";
+import { Allnews } from "./pages/Allnews.js";
 
 function App() {
- 
- return (
-   <> 
-   <ThemeProvider theme={theme}>
-     <CssBaseline>
-      <BrowserRouter>
-      
-      <Routes>
-       
-       <Route path="/" element={<><Navbar /><Home /><Footer /></>} />
-       <Route path="*" element={<NotFound></NotFound>} />
-       <Route path="/compose" element={<ComposeScoopPage></ComposeScoopPage>}/>
-       <Route path="/signin" element={<Signinform></Signinform>}/>
-    
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Navbar />
+                    <Home />
+                    <Footer />
+                  </>
+                }
+              />
+              <Route path="*" element={<NotFound></NotFound>} />
+              <Route
+                path="/compose"
+                element={<ComposeScoopPage></ComposeScoopPage>}
+              />
+              <Route path="/signin" element={<Signinform></Signinform>} />
 
-       
-        <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<AccountManagementPage/>} />
-      </Routes>
-      </BrowserRouter>
-      </CssBaseline>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/allnews" element={<Allnews/>} />
+              <Route path="/account" element={<AccountManagementPage />} />
+            </Routes>
+          </BrowserRouter>
+        </CssBaseline>
       </ThemeProvider>
-   
-   </>
- );
+    </>
+  );
 }
-
 
 export default App;
