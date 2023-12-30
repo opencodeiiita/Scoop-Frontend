ef3cc73a3a26be66baf9da83080417398b8457f5
Kushal Bansal <321kushalbansal.kb@gmail.com>
2023-12-30 21:39:51 +0530

    corrected userSlice  path

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchUserById } from './userSlice';
+import { fetchUserById } from '../redux/userSlice';
 import './Profile.css';
 
 const Profile = () => {
